79a66f7a3260ce877bfd3fdc495972ad8400b652
sadanandu <svupase@gmail.com>
2018-01-18 00:06:46 +0530

    Cross validating with kfold for classification algos

--- Titanic Passangers/PredictTitanicPassangerDeath.py
+++ Titanic Passangers/PredictTitanicPassangerDeath.py
@@ -7,12 +7,16 @@ Created on Sat Jan 13 22:25:47 2018
 
 import pandas
 import numpy
-from sklearn.linear_model import LinearRegression, LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, Imputer
-
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 
 # TransformerMixin is base class for all sklearn transformers
 from sklearn.base import TransformerMixin
@@ -89,27 +93,56 @@ def handle_categorical_data(dataframe):
     categorical_columns = ['Pclass', 'Sex', 'SibSp', 'Parch', 'Embarked']
     
     for each_column in categorical_columns:
-        print(each_column)
+        #print(each_column)
         label_encoder = LabelEncoder()
         dataframe[each_column] = label_encoder.fit_transform(dataframe[each_column])
     # categorical_features here is list of all column indexes
     # so if categorical_columns above change then this list will also
     # change
-    print(dataframe.columns)
+    #print(dataframe.columns)
     onehotencoder_obj = OneHotEncoder(categorical_features = [1, 2, 4, 5, 7])
     numpy_array = onehotencoder_obj.fit_transform(dataframe).toarray()
     #print(type(dataframe))
     return pandas.DataFrame(numpy_array)
 
-def main():
-    file_path = "C:\\Machine Learning\\MLProblems\\Titanic Passangers\\train.csv"
-    dataframe = pandas.read_csv(file_path)
-    Y, X = get_independent_and_depedent_variables(dataframe)
-    #print(dataframe.isnull().sum())
-    X = remove_unnecessary_data(X)
-    X = handle_missing_data(X)
-    X = handle_categorical_data(X)
-    print(X.columns)
-    #print(X)
+file_path = "C:\\Machine Learning\\MLProblems\\Titanic Passangers\\train.csv"
+dataframe = pandas.read_csv(file_path)
+Y, X = get_independent_and_depedent_variables(dataframe)
+#print(dataframe.isnull().sum())
+X = remove_unnecessary_data(X)
+X = handle_missing_data(X)
+X = handle_categorical_data(X)
 
-main()
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .2, random_state = 7)
+#print(X)
+
+#No need of feature scaling as of now , because most of the variables are catogrical
+# and they are handled. Only not categorical variables like AGE and FARE are remaining 
+
+models = ['LogisticRegression', 'GaussianNB', 'DecisionTreeClassifier', 'KNeighborsClassifier', 'LinearDiscriminantAnalysis' , 'SVC']
+seed = 7
+scoring = 'accuracy'
+
+for each_model in models:
+    classifier = globals().get(each_model)()
+    classifier.fit(X_train, Y_train)
+    Y_pred = classifier.predict(X_test)
+    kfold = KFold(n_splits=10, random_state=seed)
+    cv_results = cross_val_score(classifier, X_train, Y_train, cv =kfold, scoring = scoring)
+    msg = " %f +/-(%f)" % (cv_results.mean(), cv_results.std())
+    print(each_model + msg)
+    #print('Accuracy score for %s : %s' % (each_model, str(accuracy_score(Y_test, Y_pred))))
+
+
+#Closer to zero is better
+#this is used for regressors
+#print('Mean squared error- the mean value of the squared deviations of the predictions from the true values %s' % mean_squared_error(Y_test, Y_pred))
+
+#R-squared = Explained variation / Total variation
+#Higher R2 is usually better. R2 could be higher because
+#of the correlation in independent variables
+#  the higher the R-squared, the better the model fits your data.
+#print('R2 score %s' % r2_score(Y_test, Y_pred))
+
+
